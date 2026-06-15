e255f7a8385673e6dcce02662eaef53e1d770fc3
Juanjo081624 <figueroa.jota@gmail.com>
2026-06-15 16:35:05 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "novedades-turno-cr-rescate-cua-v1";
+const CACHE_NAME = "novedades-turno-cr-rescate-cua-v4";
 const APP_ASSETS = ["./", "./index.html", "./manifest.json"];
 self.addEventListener("install", (event) => { event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_ASSETS))); self.skipWaiting(); });
 self.addEventListener("activate", (event) => { event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))); self.clients.claim(); });
